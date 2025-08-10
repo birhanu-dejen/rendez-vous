0e797e7e240c887dc668908639e072eeaa78ce3b
birhanu-dejen <birhanudejen617@gmail.com>
2025-08-10 22:07:04 +0300

    fix the image source

--- components/landing/hero.tsx
+++ components/landing/hero.tsx
@@ -4,7 +4,7 @@ export default function HeroSection() {
   return (
     <section className="relative w-full h-[80vh]">
       <Image
-        src="/images/hero.jpg"
+        src="/images/hero/hero.jpg"
         alt="Hotel Room"
         layout="fill"
         objectFit="cover"
